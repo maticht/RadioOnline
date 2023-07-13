13023c2ea1de06106fc8505321408b3c10db06d6
Dmitry Pavlovich <dimonza49@gmail.com>
2023-07-14 01:37:58 +0300

    online done + nav bar search name bug fix + genre&country sort

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
 import RadioStationStore from "./store/RadioStationStore";
-import UserStore from "./store/UserStore";
 export const Context = createContext(null)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <Context.Provider value ={{
-       radioStation: new RadioStationStore(),
-       user: new UserStore(),
+       radioStation: new RadioStationStore()
     }}>
         <App />
     </Context.Provider>,
